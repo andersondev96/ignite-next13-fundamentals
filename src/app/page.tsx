c8db44d633c411f4bba473e38bccd9b36c005bac
Anderson Fernandes <anderson.fernandes96@outlook.com>
2024-05-10 21:14:16 -0300

    Fundamentos App Router: Suspense API no React

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,28 @@
+import { Suspense } from 'react'
+
+import { GithubProfile } from '@/components/github-profile'
+import { LongWaitComponent } from '@/components/long-wait-component'
+
 export default async function Home() {
-  await new Promise((resolve) => setTimeout(resolve, 2000))
+  await new Promise((resolve) => setTimeout(resolve, 1000))
 
-  const response = await fetch('https://api.github.com/users/andersondev96')
+  return (
+    <div>
+      <h1>Home</h1>
 
-  const user = await response.json()
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt nam modi
+        cupiditate sapiente dolorum ad quas veritatis nesciunt, distinctio ex
+        adipisci delectus quod aut enim porro quaerat. Ullam, esse asperiores.
+      </p>
 
-  return <pre>{JSON.stringify(user, null, 2)}</pre>
+      <Suspense fallback={<p>Carregando LongWaitComponent</p>}>
+        <LongWaitComponent />
+      </Suspense>
+
+      <Suspense fallback={<p>Carregando GithubProfile</p>}>
+        <GithubProfile />
+      </Suspense>
+    </div>
+  )
 }
